f8c46047412c4789194e7f4ea1f00953a01ece7a
Jonathan Andres <jcabas.ing@gmail.com>
2016-03-09 10:17:45 -0500

    test routes

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-profile = require("./profile");
-var username = "jcabasc";
-var users = ["davemcfarland", "chalkers", "jcabasc", "gestarita"]
-users.forEach(profile.get);
+// create web server
+
+var http = require('http');
+
+http.createServer(function(request, response){
+    response.writeHead(200, {'Content-Type':'text/plain'});
+    response.write("Before end\n");
+    response.end('Hello world\n');
+    response.write("After end\n");
+}).listen(3000);
+console.log('Server is running at local');
+
+
+function homeRoute(request, response){
+    response.writeHead(200, {'Content-Type':'text/plain'});
+    response.write("Header\n");
+    response.write("Search\n");
+    response.write("footer\n");
+}
---
+++ test/app.js
@@ -0,0 +1,4 @@
+profile = require("./profile");
+var username = "jcabasc";
+var users = ["davemcfarland", "chalkers", "jcabasc", "gestarita"]
+users.forEach(profile.get);
