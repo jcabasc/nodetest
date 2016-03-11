36e95c2d9aa228d2de8ea2a8a7b54fb005d56b2e
Jonathan Cabas <jcabas.ing@gmail.com>
2016-03-11 00:31:18 -0500

    set content-type as html

--- app.js
+++ app.js
@@ -9,9 +9,8 @@ http.createServer(function(request, response){
         response.end();
         return;
     }
-    response.writeHead(200, {'Content-Type':'text/plain'});
+    response.writeHead(200, {'Content-Type':'text/html'});
     router.homeRoute(request, response);
     router.userRoute(request, response);
-    // response.end();
 }).listen(3000);
 console.log('Server is running at local');
